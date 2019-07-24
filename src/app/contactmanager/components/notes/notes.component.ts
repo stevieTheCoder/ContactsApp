bdc98cdc75e58675f64cfe6623fb82c49f87c1bd
Steven Rogers <steven.rogers1990@gmail.com>
2019-07-24 21:08:19 +0100

    sorting and filtering

--- src/app/contactmanager/components/notes/notes.component.ts
+++ src/app/contactmanager/components/notes/notes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { Note } from '../../models/note';
 import {MatTableDataSource} from '@angular/material/table';
-import { MatPaginator } from '@angular/material';
+import { MatPaginator, MatSort } from '@angular/material';
 
 @Component({
   selector: 'app-notes',
@@ -12,6 +12,7 @@ export class NotesComponent implements OnInit {
 
   @Input() notes: Note[];
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
 
   displayedColumns: string[] = ['position', 'title', 'date'];
   dataSource: MatTableDataSource<Note>;
@@ -21,5 +22,12 @@ export class NotesComponent implements OnInit {
   ngOnInit() {
     this.dataSource = new MatTableDataSource<Note>(this.notes);
     this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue
+    .trim()
+    .toLowerCase();
   }
 }
